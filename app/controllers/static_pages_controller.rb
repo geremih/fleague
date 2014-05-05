7d55962e22f30df40c71455e92588d484c9382ed
Mihir Rege <mihirrege@gmail.com>
2014-05-05 18:21:47 +0530

    Change devise views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user!
   def home
+    @players = Player.all
+
+    
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
+
+  #TODO: Hacky way for now, fix later
+  root "static_pages#home"
+
+
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
