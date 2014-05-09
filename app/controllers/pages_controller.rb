f24a71ad66d46222152d649c4f6e814e55aeaf5e
Mihir Rege <mihirrege@gmail.com>
2014-05-09 15:43:41 +0530

    Debug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
   def home
-    @teams = current_user.teams.select {|t| Match.find(t.match_id).completed}
+    @teams = current_user.teams
     @users = User.all.sort{ |a,b|  b.score - a.score}
     @upcoming_matches = Match.where(completed: nil)
   end
