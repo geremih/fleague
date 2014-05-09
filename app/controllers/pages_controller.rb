25519af5f9644194ba96cfad3c3f07ad8b50602d
Mihir Rege <mihirrege@gmail.com>
2014-05-09 14:37:01 +0530

    Add leaderboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
   def home
+    @teams = current_user.teams.select {|t| Match.find(t.match_id).completed}
+    @users = User.all.sort{ |a,b|  b.score - a.score}
+    @upcoming_matches = Match.where(completed: nil)
   end
 end
